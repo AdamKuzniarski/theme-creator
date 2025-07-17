563e28ed430aea5252d46620a1e9c834cad632dd
AdamKuzniarski <adam.kuzniarski@gmx.de>
2025-07-17 11:11:14 +0200

    add edit function, fix bug at theme

--- src/components/Theme/Theme.jsx
+++ src/components/Theme/Theme.jsx
@@ -1,47 +1,42 @@
 import "./Theme.css";
 
 import ColorCard from "../ColorCard";
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import EditForm from "../EditForm/EditForm";
+
+const DISPLAY_STATES = {
+  preview: "preview",
+  detail: "detail",
+  edit: "edit",
+};
+//ENUM
 
 export default function Theme({ theme, onDelete, onUpdate }) {
   //lokaler State für zusammenklappen!
-  const [open, setOpen] = useState(true);
-  const [edit, setEdit] = useState(false);
-  const [editTheme, setEditTheme] = useState(false);
+
+  const [display, setDisplay] = useState(DISPLAY_STATES.preview);
 
   // wemm soch prop 'theme' ändert wird synchronisiert
-  useEffect(() => {
-    setEditTheme(theme);
-  }, [theme]);
+
   //funktion die dreht die State open und !open => wenn open dreh den State um
   function handleToogle() {
-    setOpen((open) => !open);
+    setDisplay(
+      display === DISPLAY_STATES.preview
+        ? DISPLAY_STATES.detail
+        : DISPLAY_STATES.preview
+    );
   }
-//edit modus wechseln
+  //edit modus wechseln
   function handleEdit() {
-    setEdit(true);
+    setDisplay(DISPLAY_STATES.edit);
   }
-// speichet Änderungen und verlässt EditModus
-  function handleSave() {
-    onUpdate(editTheme);
-    setEdit(false);
-  }
-
-  function handleChangeName(event) {
-    const name = event.target.value;
-    setEditTheme((editTheme) => ({ ...editTheme, name }));
+  // speichet Änderungen und verlässt EditModus
+  function handleUpdateTheme(updatedTheme) {
+    onUpdate(updatedTheme);
+    setDisplay(DISPLAY_STATES.detail);
   }
 
-// index track welche Farbe im Array betroffen wird
-
-  function handleColorChange(index, newValue) {
-    // array kopieren
-    const newColors = [...editTheme.colors];
-    // eim eomzelnes Objekt aktualiseiern
-    newColors[index] = { ...newColors[index], value: newValue };
-    //State updaten + render
-    setEditTheme((editTheme) => ({ ...editTheme, colors: newColors }));
-  }
+  // index track welche Farbe im Array betroffen wird
 
   return (
     <section className="theme-section">
@@ -65,65 +60,24 @@ export default function Theme({ theme, onDelete, onUpdate }) {
         <span className="theme-arrow">{open ? "▼" : "▲"}</span>
       </div>
 
-      {open && (
+      {display === DISPLAY_STATES.detail && (
         <div className="theme-detail">
-          {!edit ? (
-            <>
-              {theme.colors.map((color) => (
-                <ColorCard
-                  key={color.role}
-                  role={color.role}
-                  hex={color.value}
-                />
-              ))}
-              <button type="button" onClick={handleEdit}>
-                Edit
-              </button>
-              <button
-                className="theme-delete-button"
-                onClick={() => onDelete(theme.id)}
-              >
-                Delete Theme
-              </button>
-            </>
-          ) : (
-            <>
-              <label>
-                Name:
-                <input
-                  type="text"
-                  value={editTheme.name}
-                  onChange={handleChangeName}
-                />
-              </label>
-              {editTheme.colors.map((color, index) => (
-                <div key={color.role} className="edit-row">
-                  {color.role}
-                  <input
-                    type="color"
-                    value={color.value}
-                    onChange={(event) =>
-                      handleColorChange(index, event.target.value)
-                    }
-                  />
-                  <input
-                    type="text"
-                    value={color.value}
-                    onChange={(event) =>
-                      handleColorChange(index, event.target.value)
-                    }
-                  />
-                </div>
-              ))}
-              <button type="button" onClick={handleSave}>
-                Save Theme
-              </button>
-              <button type="button" onClick={() => setEdit(false)}>
-                Cancel
-              </button>
-            </>
-          )}
+          {theme.colors.map((color) => (
+            <ColorCard key={color.role} role={color.role} hex={color.value} />
+          ))}
+          <button type="button" onClick={handleEdit}>
+            Edit
+          </button>
+          <button
+            className="theme-delete-button"
+            onClick={() => onDelete(theme.id)}
+          >
+            Delete Theme
+          </button>
         </div>
+      )}
+      {display === DISPLAY_STATES.edit && (
+        <EditForm onSubmit={handleUpdateTheme} theme={theme} />
       )}
     </section>
   );
